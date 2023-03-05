dd5ef509131afb03ed390ff52d6725feaf657636
NamerPRO <mail@namerpro.me>
2023-03-05 21:43:40 +0300

    Выполненное задание спринт 11. Попытка 1.

--- app/src/main/java/ru/namerpro/playlistmaker/SearchActivity.kt
+++ app/src/main/java/ru/namerpro/playlistmaker/SearchActivity.kt
@@ -1,17 +1,30 @@
 package ru.namerpro.playlistmaker
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
 import android.view.inputmethod.EditorInfo
-import android.widget.EditText
-import android.widget.ImageView
+import android.widget.*
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class SearchActivity : AppCompatActivity() {
+
+    private val retrofit = Retrofit.Builder()
+        .baseUrl("https://itunes.apple.com")
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    private lateinit var savedRequest: String
+    private val itunesService = retrofit.create(ItunesServiceApi::class.java)
 
     var searchDataValue = ""
 
@@ -30,6 +43,7 @@ class SearchActivity : AppCompatActivity() {
         findViewById<EditText>(R.id.search_area).setText(searchDataValue)
     }
 
+    @SuppressLint("NotifyDataSetChanged")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
@@ -41,10 +55,21 @@ class SearchActivity : AppCompatActivity() {
 
         val searchArea = findViewById<EditText>(R.id.search_area)
 
+        val trackView = findViewById<RecyclerView>(R.id.track_view)
+        trackView.layoutManager = LinearLayoutManager(this)
+
+        val trackList = mutableListOf<Track>()
+
+        val trackAdapter = TrackAdapter(trackList)
+        trackView.adapter = trackAdapter
+
         val clearTextButton = findViewById<ImageView>(R.id.search_clear_text)
         clearTextButton.setOnClickListener {
             searchArea.setText("")
             searchArea.onEditorAction(EditorInfo.IME_ACTION_DONE)
+            trackList.clear()
+            trackAdapter.notifyDataSetChanged()
+            trackView.visibility = View.GONE
         }
 
         searchArea.addTextChangedListener(object : TextWatcher {
@@ -57,17 +82,57 @@ class SearchActivity : AppCompatActivity() {
             }
         })
 
-        val trackView = findViewById<RecyclerView>(R.id.track_view)
-        trackView.layoutManager = LinearLayoutManager(this)
+        val nothingFound = findViewById<FrameLayout>(R.id.nothing_found)
+        val noInternet = findViewById<LinearLayout>(R.id.nointernet)
 
-        val trackList : List<Track> = listOf(Track("Smells Like Teen Spirit", "Nirvana", "5:01", "https://is5-ssl.mzstatic.com/image/thumb/Music115/v4/7b/58/c2/7b58c21a-2b51-2bb2-e59a-9bb9b96ad8c3/00602567924166.rgb.jpg/100x100bb.jpg"),
-                                            Track("Billie Jean", "Michael Jackson", "4:35", "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/3d/9d/38/3d9d3811-71f0-3a0e-1ada-3004e56ff852/827969428726.jpg/100x100bb.jpg"),
-                                            Track("Stayin' Alive", "Bee Gees", "4:10", "https://is4-ssl.mzstatic.com/image/thumb/Music115/v4/1f/80/1f/1f801fc1-8c0f-ea3e-d3e5-387c6619619e/16UMGIM86640.rgb.jpg/100x100bb.jpg"),
-                                            Track("Whole Lotta Love", "Led Zeppelin", "5:33", "https://is2-ssl.mzstatic.com/image/thumb/Music62/v4/7e/17/e3/7e17e33f-2efa-2a36-e916-7f808576cf6b/mzm.fyigqcbs.jpg/100x100bb.jpg"),
-                                            Track("Sweet Child O'Mine", "Guns N' Roses", "5:03", "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg"))
+        val itunesCallback = object : Callback<TrackResponse> {
 
-        val trackAdapter = TrackAdapter(trackList)
-        trackView.adapter = trackAdapter
+            override fun onResponse(
+                call: Call<TrackResponse>,
+                response: Response<TrackResponse>
+            ) {
+                if (response.code() == 200) {
+                    noInternet.visibility = View.GONE
+                    nothingFound.visibility = View.GONE
+                    trackView.visibility = View.GONE
+                    trackList.clear()
+                    if (response.body()?.results?.isNotEmpty() == true) {
+                        trackList.addAll(response.body()?.results!!)
+                        trackView.visibility = View.VISIBLE
+                        trackAdapter.notifyDataSetChanged()
+                    }
+                    if (trackList.isEmpty()) {
+                        nothingFound.visibility = View.VISIBLE
+                    }
+                } else {
+//                                Toast.makeText(applicationContext, "Failed to download content :(", Toast.LENGTH_LONG).show()
+                    noInternet.visibility = View.VISIBLE
+                }
+            }
+
+            override fun onFailure(call: Call<TrackResponse>, t: Throwable) {
+                trackView.visibility = View.GONE
+                nothingFound.visibility = View.GONE
+                noInternet.visibility = View.VISIBLE
+            }
+
+        }
+
+        val updateContent = findViewById<Button>(R.id.search_update_content)
+        updateContent.setOnClickListener {
+            itunesService.search(savedRequest).enqueue(itunesCallback)
+        }
+
+        searchArea.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                if (searchArea.text.isNotEmpty()) {
+                    savedRequest = searchArea.text.toString()
+                    itunesService.search(searchArea.text.toString()).enqueue(itunesCallback)
+                }
+                true
+            }
+            false
+        }
 
     }
 }
